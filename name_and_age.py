2c9d92f2ec9b165b866e202bf897a374ad73cb56
Gabriel <you@example.com>
2024-10-22 00:39:03 +0800

    I used dictionary array and It prints the collected data

--- name_and_age.py
+++ name_and_age.py
@@ -6,14 +6,20 @@ while True:
     name = input("Pls input name: ")
    
     if len(name) == 1:
-        print("Error: Name must not be equal to 1 letter.")   
-        
+        print("Error: Name must not be equal to 1 letter.")  
+
     age = int(input("Pls input age: "))
 
     if age < 0 or age > 120:
         print ("Error: Age must be number between 0 and 120.")
 
+    name_and_age = {
+        "name" : name,
+        "age" : age
+    }
 
+    print("Collected Data:")
+    print(f"Name: {name_and_age['name']}, Age: {name_and_age['age']}")
 
 
     
