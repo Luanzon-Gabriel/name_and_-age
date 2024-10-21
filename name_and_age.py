3fcfb6c08b67afedcaf125460ad5150e55285ac3
Gabriel <you@example.com>
2024-10-21 23:41:05 +0800

    I put a definition of a valid name

--- name_and_age.py
+++ name_and_age.py
@@ -5,5 +5,10 @@ while True:
 
     name = input("Pls input name: ")
     age = int(input("Pls input age: "))
+        
+    if len(name) == 1:
+        print("Error: Name must not be equal to 1 letter.")
+
+
 
     
