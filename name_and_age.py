60d039268dd8f5e36dfe582e632ec0901f4681e1
Gabriel <you@example.com>
2024-10-22 02:48:20 +0800

    I added a function on how to get the oldest and I put print the oldest so that we can see who and how old is the oldest

--- name_and_age.py
+++ name_and_age.py
@@ -21,14 +21,28 @@ while True:
     retry = input("Input again? (Yes/No) ")
             
     if retry == "No":
-         break
-                
+         break            
 
     name_and_age["name"] = name
 
     name_and_age["age"] = age
 
-    all_input += [name_and_age]
+    all_input.append(name_and_age)
 
+def get_oldest_person(name_and_age_list):
 
-           
+    oldest_person = name_and_age_list[0]
+
+    for person in name_and_age_list:
+
+        if person["age"] > oldest_person["age"]:
+
+            oldest_person = person
+
+        return oldest_person
+
+oldest_person = get_oldest_person(all_input)
+
+print("The oldest person is:")
+
+print(f"name: {oldest_person['name']}, age: {oldest_person['age']}")
