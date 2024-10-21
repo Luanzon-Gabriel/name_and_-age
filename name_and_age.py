c4005a76dad1af2ea621dec226b78f0533f31d33
Gabriel <you@example.com>
2024-10-22 00:11:30 +0800

    I put together the name input and valid checker, same as the age so that when there is an error you will be reminded right away

--- name_and_age.py
+++ name_and_age.py
@@ -4,10 +4,11 @@ name_and_age = {}
 while True:
 
     name = input("Pls input name: ")
-    age = int(input("Pls input age: "))
-        
+   
     if len(name) == 1:
-        print("Error: Name must not be equal to 1 letter.")
+        print("Error: Name must not be equal to 1 letter.")   
+        
+    age = int(input("Pls input age: "))
 
     if age < 0 or age > 120:
         print ("Error: Age must be number between 0 and 120.")
